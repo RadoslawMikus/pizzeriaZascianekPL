fae8d599e60262c4919cb2bed8add25768e26087
davidsmith931 <radoslaw.mikus@gmail.com>
2022-05-03 19:39:52 +0200

    Added full pizzaOfTheDay module

--- script/main.js
+++ script/main.js
@@ -47,6 +47,162 @@ const pizzaDiscount = {
     price50: "30",
     price60: "36",
   },
+  6: {
+    name: "Pieczarkowa",
+    price30: "14",
+    price50: "27",
+    price60: "34",
+  },
+  7: {
+    name: "Wiejska",
+    price30: "17",
+    price50: "32",
+    price60: "41",
+  },
+  8: {
+    name: "Grillowany kurczak",
+    price30: "15",
+    price50: "30",
+    price60: "36",
+  },
+  9: {
+    name: "Włoska",
+    price30: "16",
+    price50: "31",
+    price60: "38",
+  },
+  10: {
+    name: "Ogórkowa",
+    price30: "17",
+    price50: "32",
+    price60: "41",
+  },
+  11: {
+    name: "Kebabowa",
+    price30: "16",
+    price50: "31",
+    price60: "38",
+  },
+  12: {
+    name: "Meksykańska",
+    price30: "17",
+    price50: "32",
+    price60: "41",
+  },
+  13: {
+    name: "Piekielna",
+    price30: "17",
+    price50: "32",
+    price60: "41",
+  },
+  14: {
+    name: "Szpinakowa",
+    price30: "17",
+    price50: "32",
+    price60: "41",
+  },
+  15: {
+    name: "Serowa",
+    price30: "16",
+    price50: "31",
+    price60: "38",
+  },
+  16: {
+    name: "Amerykańska",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
+  17: {
+    name: "Kurczak słodki chilli",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
+  18: {
+    name: "Wiosenna",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
+  19: {
+    name: "Gangsterska",
+    price30: "17",
+    price50: "32",
+    price60: "41",
+  },
+  20: {
+    name: "Rzymska",
+    price30: "15",
+    price50: "30",
+    price60: "36",
+  },
+  21: {
+    name: "Brokułowa",
+    price30: "17",
+    price50: "32",
+    price60: "41",
+  },
+  22: {
+    name: "Z owocami morza",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
+  23: {
+    name: "Mięsna uczta",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
+  24: {
+    name: "Kurczak na Hawajach",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
+  25: {
+    name: "Pepperoni",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
+  26: {
+    name: "Kurczak BBQ",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
+  27: {
+    name: "Parmeńska",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
+  28: {
+    name: "Góralska",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
+  29: {
+    name: "Capricciosa",
+    price30: "15",
+    price50: "30",
+    price60: "36",
+  },
+  30: {
+    name: "Wiejska",
+    price30: "17",
+    price50: "32",
+    price60: "41",
+  },
+  31: {
+    name: "Mięsna uczta",
+    price30: "19",
+    price50: "36",
+    price60: "42",
+  },
 };
 
 const today = String(new Date().getDate());
@@ -56,10 +212,6 @@ const todaysDiscount = document.createElement("div");
 for (let i = 1; i <= Object.keys(pizzaDiscount).length; i++) {
   if (today == i) {
     todaysDiscount.innerHTML = `${pizzaDiscount[i]["name"]}, ${pizzaDiscount[i]["price30"]}, ${pizzaDiscount[i]["price50"]}, ${pizzaDiscount[i]["price60"]} `;
-    console.log(pizzaDiscount[i]["name"]);
-    console.log(pizzaDiscount[i]["price30"] + " zł");
-    console.log(pizzaDiscount[i]["price50"] + " zł");
-    console.log(pizzaDiscount[i]["price60"] + " zł");
   }
 }
 console.log("Today is: " + today);
@@ -84,3 +236,16 @@ buttonOthers.addEventListener("click", () => {
   menuOthers.classList.remove("d-none");
   menuPizza.classList.add("d-none");
 });
+
+// ----------------------------------
+// SMOOTH SCROLL
+// ----------------------------------
+
+document.querySelector("html").style.scrollBehavior = "auto";
+const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+  speed: 1200,
+  speedAsDuration: true,
+  header: "[data-scroll-header]",
+});
+
+console.log(pizzaDiscount);

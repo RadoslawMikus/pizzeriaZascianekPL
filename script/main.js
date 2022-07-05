7437ee9d3b1300c92f4fbbe284fe9ea0362658f5
davidsmith931 <radoslaw.mikus@gmail.com>
2022-07-05 21:52:02 +0200

    Improved code in main.js

--- script/main.js
+++ script/main.js
@@ -1,21 +1,19 @@
 // ----------------------------------
 // CHANGE NAVBAR TO BLACK ON SCROLL
 // ----------------------------------
-
 const navbar = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 150) {
-    navbar.style.backgroundColor = "black";
+    navbar.setAttribute("id", "bg-scroll");
   } else {
-    navbar.style.backgroundColor = "";
+    navbar.removeAttribute("id");
   }
 });
 
 // ----------------------------------
 // PIZZA OF A DAY
 // ----------------------------------
-
 const pizzaDiscount = {
   1: {
     name: "Margherita",
@@ -214,38 +212,32 @@ for (let i = 1; i <= Object.keys(pizzaDiscount).length; i++) {
     todaysDiscount.innerHTML = `<table class="pizzaOfTheDay"><tbody><tr><th colspan="100">PIZZA DNIA</th></tr><tr><td>${pizzaDiscount[i]["name"]}</td> <td>${pizzaDiscount[i]["price30"]} zł</td> <td>${pizzaDiscount[i]["price50"]} zł</td> <td>${pizzaDiscount[i]["price60"]} zł</td></tr></tbody></table> `;
   }
 }
-console.log("Today is: " + today);
 
 discountDiv.prepend(todaysDiscount);
 
 // ----------------------------------
 // TOGGLE MENU
 // ----------------------------------
-
 const buttonPizza = document.querySelector("#buttonPizza");
 const buttonOthers = document.querySelector("#buttonOthers");
 const menuPizza = document.querySelector(".menuPizza");
 const menuOthers = document.querySelector(".menuOthers");
 
-buttonPizza.addEventListener("click", () => {
+const toggler = () => {
   menuOthers.classList.add("d-none");
   menuPizza.classList.remove("d-none");
-});
+};
 
-buttonOthers.addEventListener("click", () => {
-  menuOthers.classList.remove("d-none");
-  menuPizza.classList.add("d-none");
-});
+[buttonPizza, buttonOthers].forEach((button) =>
+  button.addEventListener("click", toggler)
+);
 
 // ----------------------------------
 // SMOOTH SCROLL
 // ----------------------------------
-
 document.querySelector("html").style.scrollBehavior = "auto";
 const scroll = new SmoothScroll('.navbar a[href*="#"]', {
   speed: 1200,
   speedAsDuration: true,
   header: "[data-scroll-header]",
 });
-
-console.log(pizzaDiscount);
